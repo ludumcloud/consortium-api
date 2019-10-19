0f7371cb2c593000f33cd5f0ae35d55b1b4e38c1
Arin Reese <arin.reese@genesys.com>
2019-10-18 23:47:13 -0400

    remove hello world example

--- src/RestModule.ts
+++ src/RestModule.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import AuthController from './controllers/AuthController';
-import { HelloWorldController} from './controllers/HelloWorldController';
 
 @Module({
   imports: [],
   controllers: [
-    AuthController,
-    HelloWorldController
+    AuthController
   ],
   providers: [],
 })
